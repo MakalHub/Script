55bdc58dbcd595f7d69cd154069a64d0c7c502b3
MakalHub <michaeldolii1603@gmail.com>
2026-01-17 21:56:18 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 131623223084840 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/MakalHub/Script/refs/heads/main/serverfinder.lua"))()
+end
+print("Made by @decomp1le")
