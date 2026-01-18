3454c4d52b38bcd9979bcc6bd5a402604550a360
MakalHub <michaeldolii1603@gmail.com>
2026-01-18 17:22:27 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 131623223084840 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/MakalHub/Script/refs/heads/main/serverfinder.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/MakalHub/Script/refs/heads/main/etfb.lua"))()
 end
 print("Made by @decomp1le")
